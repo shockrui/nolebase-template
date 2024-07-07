cc1b4f23bfa695565edb2f9ebde31e2a8d8e0621
shockrui <1206640240@qq.com>
2024-07-07 10:19:57 +0800

    update

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,6 +1,6 @@
 /** 文本 */
-export const siteName = 'Nólëbase'
-export const siteShortName = 'Nólëbase'
+export const siteName = 'shockrui'
+export const siteShortName = 'shockrui'
 export const siteDescription = '记录回忆，知识和畅想的地方'
 
 /** 文档所在目录 */
